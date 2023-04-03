e9c3c0027f0e6153f24688731c7e84a3391d4b57
jadonvez <terukaaa69@gmail.com>
2023-04-03 19:19:27 +0200

    push avril

--- queries.js
+++ queries.js
@@ -1,12 +1,13 @@
 const getUsers = "SELECT * FROM users";
 const getUsersById = "SELECT * FROM users WHERE id = $1";
+const getUsersByUid = "SELECT * FROM users WHERE uid = $1";
 const checkMailExists = "SELECT s FROM users s WHERE s.mail = $1";
 
 const checkMailPasswordExists = "SELECT * FROM users WHERE mail = $1 AND password = $2";
 
 const addUser = "INSERT INTO users (name, firstname, mail, password, dob, gender) VALUES ($1, $2, $3, $4, $5, $6)";
 const removeUser = "DELETE FROM users WHERE id = $1";
-const updateUser = "UPDATE users SET name = $1 WHERE id = $2";
+const updateUser = "UPDATE users SET bio = $1 WHERE id = $2";
 
 module.exports = {
     getUsers,
@@ -16,5 +17,6 @@ module.exports = {
     removeUser,
     updateUser,
     checkMailPasswordExists,
+    getUsersByUid,
     //checkPasswordExists,
 };
