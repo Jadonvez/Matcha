81515b50e7f2805e25b971b3fb922a434b812571
jadonvez <terukaaa69@gmail.com>
2023-04-03 20:07:56 +0200

    middleware avril

--- middleware/check_mail.js
+++ middleware/check_mail.js
@@ -4,14 +4,20 @@ module.exports = (req, res, next) => {
     try{
         console.log("----> middleware check_mail");
         console.log(req.body.mail);
-        //console.log(req.body.login);
         pool.query("SELECT * FROM users WHERE mail = $1", [req.body.mail], 
         (error, results) =>{
         if (error)
+        {
+            console.log("erreur 401 check mail middleware");
             res.status(401).json({ error : error })
+        }
         else if (results.rows.length)
-            res.send("Le mail existe deja.");
-        else next();
+            res.status(401).json({ error: "Le mail existe deja" });
+        else
+        {
+            console.log("ca passe le check mail middleware");
+            next();
+        }
         });
     }catch(error){
         res.status(401).json({ error : "middleware mail" });
--- middleware/jwtcheck.js
+++ middleware/jwtcheck.js
@@ -1,23 +1,76 @@
 const jwt = require('jsonwebtoken');
 const cookie = require('cookie-parser');
 
-const loginrequired = async (req, res, next) => {
-    const token = req.cookies['acces-token']
+
+const checkUser = (req, res, next) => {
+    const token = req.cookies['access-token'];
     if (token) {
-        const validatetoken = await jwt.verify(token, process.env.TOKEN_SECRET)
-        if (validatetoken) {
-            //res.user = validatetoken.id
-            console.log('req.body de jwtcheck');
-            console.log(req.body);
-            next()
-        }
-        else {
-            console.log('token expires => jwtcheck');
-        }
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, mailID) => {
+            if (err) {
+                console.log("erreur dans le checkUser");
+                res.locals.user = null;
+                //res.cookie('jwt', '', { maxAge: 1 });
+                next();
+            }
+            else {
+                let user = mailID.id;
+                res.locals.user = user;
+                //console.log("checkUser console log !!!!!!!!!!")
+                //console.log(user);
+                //console.log(res.locals.user);
+                next();
+            }
+        })
     }
     else {
-        console.log('token not found => jwtcheck');
+        res.locals.user = null;
+        next();
     }
 }
 
-module.exports = { loginrequired }
+const loginrequired = (req, res, next) => {
+    const token = req.cookies['access-token'];
+    //const authHeader = req.headers.authorization;
+    //const token3 = req.headers['authorization'];
+    //const token2 = authHeader.split(' ')[1]; 
+    ////console.log("=====> token" ,token);
+    //console.log("====> autHeader", authHeader);
+    //console.log("====> auth2 Header", token3);
+    //console.log("====> token auth[1]", token2);
+
+    // if (token) {
+    //     const validatetoken = jwt.verify(token, process.env.TOKEN_SECRET)
+    //     if (error) {
+    //         res.status(403).json('Invalid token');
+    //     }
+    //     else if (validatetoken) {
+    //         //res.user = validatetoken.id
+    //         console.log('req.body de jwtcheck');
+    //         console.log(req.body);
+    //         next()
+    //     }
+    //     else {
+    //         res.send('token expires => jwtcheck');
+    //         //console.log('token expires => jwtcheck');
+    //     }
+    // }
+    // else {
+    //     res.send('token not found => jwtcheck');
+    //     //console.log('token not found => jwtcheck');
+    //     // console.log('req.body de jwtcheck');
+    //     // console.log(req.mail);
+    // }
+    if (!token) {
+        return res.status(401).json('No token found');
+    }
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, mailID) => {
+        if (err) {
+            return res.status(403).json('Invalid Token');
+        }
+        //console.log(mailID.id);
+        next();
+    })
+}
+
+module.exports = { loginrequired, checkUser }
